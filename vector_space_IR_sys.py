df98199a40d0268d23ae6d6dc51a80c4d6bf8283
Charic Daniel Farinango Cuervo <charicfc@gmail.com>
2020-05-09 13:18:43 -0500

    VSM: changes in query weights to improve precission and recall as baseline

--- vector_space_IR_sys.py
+++ vector_space_IR_sys.py
@@ -12,8 +12,6 @@ def tokenize_docs(documents):
 		doc_tokens = get_tokens_by_space(doc)
 		preprocess_doc = preprocessing(doc_tokens)
 		
-		#token_freq_doc = get_TF_values(preprocess_doc)
-
 		tokenized_docs.append(preprocess_doc)
 
 	return tokenized_docs
@@ -27,45 +25,53 @@ def preprocessing(tokens):
 
 	return final_words
 
-def get_TF_values(toeknized_doc): # Receives a list of dictionaries
-
-	pdb.set_trace()
-	token_freq = FreqDist(toeknized_doc).most_common(1)[0][1]
-	return token_freq
-	#pdb.set_trace()
-	# TF_doc_collection = []
-	# for document_dic in doc_collection:
-	# 	total = max(document_dic.values()) # Maximum frequency in the document
-	# 	if total != 0:
-	# 		document_dic = {word: freq / total for word, freq in document_dic.items()}
-	# 	#pdb.set_trace()
-
-		
-	# 	TF_doc_collection.append(document_dic)
-
-	# return TF_doc_collection
-
-def index_docs(tokenized_docs, query=False):
+def index_docs(tokenized_docs):
 
 	docs_index = {}
-	most_freq_term = 1
 
+	#pdb.set_trace()
 	for doc_id, doc in enumerate(tokenized_docs):
+		doc_id = doc_id + 1
 
-		if not query: 
+		try:
 			most_freq_term = FreqDist(doc).most_common(1)[0][1]
+		except:
+			most_freq_term = 1
+
 		for term in doc:
 			if term in docs_index:
 				#pdb.set_trace()
 				if "d"+str(doc_id) in docs_index[term][1]:
 					docs_index[term][1]["d"+str(doc_id)] += 1/most_freq_term
 				else:
-					docs_index[term][0] = len(docs_index[term][1])
 					docs_index[term][1]["d"+str(doc_id)] = 1/most_freq_term
-
+					docs_index[term][0] = len(docs_index[term][1])
 			else:
 				# Creates term and its dictionary
 				docs_index[term] = [1, {"d"+str(doc_id): 1/most_freq_term}]
+
+	return docs_index
+
+def index_query(tokenized_docs, tokenized_query):
+
+	docs_index = {}
+
+	for doc_id, doc in enumerate(tokenized_docs):
+		doc_id = doc_id + 1
+
+		terms_freq = FreqDist(doc)
+
+		for term in tokenized_query:
+			if term in doc:
+				if term in docs_index:
+					#pdb.set_trace()
+					docs_index[term][1]["d"+str(doc_id)] = terms_freq[term]
+					docs_index[term][0] = len(docs_index[term][1])
+				else:
+					# Creates term and its dictionary
+					docs_index[term] = [1, {"d"+str(doc_id): terms_freq[term]}]
+			else:
+				continue
 
 	return docs_index
 
@@ -75,7 +81,7 @@ def get_weithgs_and_lenghts(inverted_index, N):
 	tf_idf = {}
 
 	for i in range(N):
-		doc = "d"+str(i)
+		doc = "d"+str(i+1)
 		doc_lenghts[doc] = 0
 
 		
@@ -96,31 +102,34 @@ def get_weithgs_and_lenghts(inverted_index, N):
 	return tf_idf, doc_lenghts
 
 # Performs Cosine Similarity and retrieves ranked docs
-def retrieve_similar_docs(weights, doc_lenghts, inverted_index, query_inv_index, N):
+def retrieve_similar_docs(weights, doc_lenghts, inverted_index, query_inv_index, q_lenghts, N):
 
 	cos_sim = {}
-	query_lenght = 0
+	#query_lenght = 0
 	#for query_inv_index
 
 	#pdb.set_trace()
-	for q_term, q_df_tf in query_inv_index.items():
-		if q_term not in inverted_index: continue
-		w = q_df_tf[1]['d0'] * math.log2(N/inverted_index[q_term][0])
-		query_lenght = query_lenght + w**2
-	query_lenght = math.sqrt(query_lenght)
+	# for q_term, q_df_tf in query_inv_index.items():
+	# 	if q_term not in inverted_index: continue
+	# 	w = q_df_tf[1]['d1'] * math.log2(N/inverted_index[q_term][0])
+	# 	query_lenght = query_lenght + w**2
+	# query_lenght = math.sqrt(query_lenght)
 
 	for term, df_tf in query_inv_index.items():
 
-		if term not in inverted_index: continue
+		if term not in inverted_index: 
+			print('term not in doccs', term)
+			continue
 
+		#pdb.set_trace()
 		for doc, tf in inverted_index[term][1].items():
 
 			if doc in cos_sim:
-				dot_p = inverted_index[term][1][doc] * df_tf[1]['d0'] * (math.log2(N/inverted_index[term][0]))**2
-				cos_sim[doc] += dot_p/math.sqrt(doc_lenghts[doc]*query_lenght)
+				dot_p = inverted_index[term][1][doc] * df_tf[1][doc] * (math.log2(N/inverted_index[term][0]))**2
+				cos_sim[doc] += dot_p/math.sqrt(doc_lenghts[doc]*q_lenghts[doc])
 			else:
-				dot_p = inverted_index[term][1][doc] * df_tf[1]['d0'] * (math.log2(N/inverted_index[term][0]))**2
-				cos_sim[doc] = dot_p/math.sqrt(doc_lenghts[doc]*query_lenght)
+				dot_p = inverted_index[term][1][doc] * df_tf[1][doc] * (math.log2(N/inverted_index[term][0]))**2
+				cos_sim[doc] = dot_p/math.sqrt(doc_lenghts[doc]*q_lenghts[doc])
 
 	sorted_docs = [k for k, v in sorted(cos_sim.items(), reverse=True, key=lambda item: item[1])]
 
@@ -157,7 +166,7 @@ def get_text_of_interest(file):
 	title_re = re.compile('<TITLE>(.*)</TITLE>', re.DOTALL).search(text)
 	text_re = re.compile('<TEXT>(.*)</TEXT>', re.DOTALL).search(text)
 
-	text_oi = title_re.group(1) + text_re.group(1)
+	text_oi = title_re.group(1) #+ text_re.group(1)
 
 	return text_oi
 
@@ -182,7 +191,7 @@ def run_IR_system(links, pages, query, relevance_path, n_top_docs):
 	N = len(pages)
 
 	tokenized_docs = tokenize_docs(pages)
-	inverted_index = index_docs(tokenized_docs, False)
+	inverted_index = index_docs(tokenized_docs)
 
 	tokenized_query = tokenize_docs(query)
 
@@ -192,6 +201,12 @@ def run_IR_system(links, pages, query, relevance_path, n_top_docs):
 	weights, doc_lenghts = get_weithgs_and_lenghts(inverted_index, N)
 	#sorted_docs = retrieve_similar_docs(weights, doc_lenghts, inverted_index, query_inv_index, N)
 
+	# tem = {}
+	# for doc, terms in weights.items():
+	# 	for i in sorted (weights[doc].keys()):
+	# 		tem[i] = inverted_index[i]
+	# 	pdb.set_trace()
+
 	avg_precision = 0
 	avg_recall = 0
 	relevance = get_relevance_info(relevance_path)
@@ -200,11 +215,12 @@ def run_IR_system(links, pages, query, relevance_path, n_top_docs):
 
 	for i, qu in enumerate(tokenized_query):
 		
-		query_inv_index = index_docs([qu], True)
-		sorted_docs = retrieve_similar_docs(weights, doc_lenghts, inverted_index, query_inv_index, N)
+		query_inv_index = index_query(tokenized_docs, qu)
+		q_weights, q_lenghts = get_weithgs_and_lenghts(query_inv_index, N)
+		#pdb.set_trace()
+		sorted_docs = retrieve_similar_docs(weights, doc_lenghts, inverted_index, query_inv_index, q_lenghts, N)
 
 		top_docs = sorted_docs[:n_top_docs]
-		#pdb.set_trace()
 		rel_docs_retrieved = len(set(top_docs) & set(relevance[i]))
 		rel_docs = len(relevance[i])
 		docs_ret = len(top_docs)
